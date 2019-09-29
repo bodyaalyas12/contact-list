5632d388c4596b3ff4765ce0eadb58e0a507ecf8
Oleg <kolegnat@gmail.com>
2019-09-29 20:20:11 +0300

    build

---
+++ build/precache-manifest.ffe431173098ac8add2e5a149d98b561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3080235cc5c91f6079e8c00dc0ff6cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d31797b335f8e0d827c",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e58eb9594f2b22fa3872",
+    "url": "/static/css/main.fab6bc0d.chunk.css"
+  },
+  {
+    "revision": "9d31797b335f8e0d827c",
+    "url": "/static/js/2.0ad75f82.chunk.js"
+  },
+  {
+    "revision": "e58eb9594f2b22fa3872",
+    "url": "/static/js/main.8d974e72.chunk.js"
+  },
+  {
+    "revision": "a67651d9071b56122f8b",
+    "url": "/static/js/runtime-main.0b3b2930.js"
+  },
+  {
+    "revision": "f697cb3f68a0dffa40c638051fec297c",
+    "url": "/static/media/Address_book_1.f697cb3f.jpg"
+  }
+]);
