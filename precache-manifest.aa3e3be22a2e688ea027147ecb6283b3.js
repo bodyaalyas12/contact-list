24e6beb9b69dc6b16a2d20d9142c7c521c97d99c
bohdan <bodyaalyas12@gmail.com>
2020-02-18 20:52:05 +0200

    Updates

---
+++ precache-manifest.aa3e3be22a2e688ea027147ecb6283b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db7aa1d6b05b344d4271be99c9d9e15",
+    "url": "/contact-list/index.html"
+  },
+  {
+    "revision": "fffd06c85016ef50113d",
+    "url": "/contact-list/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "fd25512c5c087847ed32",
+    "url": "/contact-list/static/css/main.b6556f70.chunk.css"
+  },
+  {
+    "revision": "fffd06c85016ef50113d",
+    "url": "/contact-list/static/js/2.10afe05d.chunk.js"
+  },
+  {
+    "revision": "f8ce2aea1c90315ba1a27e62c24979a7",
+    "url": "/contact-list/static/js/2.10afe05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd25512c5c087847ed32",
+    "url": "/contact-list/static/js/main.499e9a90.chunk.js"
+  },
+  {
+    "revision": "46d432da170340b80ec6",
+    "url": "/contact-list/static/js/runtime-main.21474574.js"
+  },
+  {
+    "revision": "f697cb3f68a0dffa40c638051fec297c",
+    "url": "/contact-list/static/media/Address_book_1.f697cb3f.jpg"
+  }
+]);
