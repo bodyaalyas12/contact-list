ced001c908701c589c60fc871c180e38c077bb8a
bohdan <bodyaalyas12@gmail.com>
2020-08-13 16:36:01 +0300

    lil bit fix

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { animated, useTransition } from 'react-spring'
+import React, {useState} from 'react'
+import {connect} from 'react-redux'
+import {animated, useTransition} from 'react-spring'
 import host from '../host'
-import { setContacts } from '../store/actionCreators'
+import {setContacts} from '../store/actionCreators'
 
 const initialForm = {
 	name: '',
@@ -12,12 +12,12 @@ const initialForm = {
 	imgPreview: ''
 }
 
-const AddNew = ({ setContacts }) => {
+const AddNew = ({setContacts}) => {
 	const [show, setShow] = useState(false)
 	const [form, setForm] = useState(initialForm)
 	const [imgPreview, setImgPreview] = useState('')
 	const animatedProps = useTransition(show, null, {
-		config: { duration: 500 },
+		config: {duration: 500},
 		from: {
 			height: 0,
 			opacity: 0
@@ -34,32 +34,32 @@ const AddNew = ({ setContacts }) => {
 	const addNewToggler = () => {
 		setShow(show => !show)
 	}
-	const handleData = ({ target }) => {
-		const { name, value } = target
+	const handleData = ({target}) => {
+		const {name, value} = target
 		setForm({
 			...form,
 			[name]: value
 		})
 	}
 	const addClickHandler = () => {
-		const fd = new FormData(form)
+		const fd = new FormData()
 		fd.append('image', form.image)
 		fd.append('form', JSON.stringify(form))
 		fetch(`${host}/contacts/create`, {
 			body: fd,
 			method: 'POST'
 		})
-			.then(response => response.json())
-			.then(() => {
-				setForm(initialForm)
-				document.querySelector('#image_input').value = null
-				setImgPreview('')
-				setContacts()
-			})
+				.then(response => response.json())
+				.then(() => {
+					setForm(initialForm)
+					document.querySelector('#image_input').value = null
+					setImgPreview('')
+					setContacts()
+				})
 	}
-	const handleImage = ({ target }) => {
+	const handleImage = ({target}) => {
 		const reader = new FileReader()
-		reader.onloadend = kek => {
+		reader.onloadend = () => {
 			setImgPreview(reader.result)
 		}
 		reader.readAsDataURL(target.files[0])
@@ -69,81 +69,81 @@ const AddNew = ({ setContacts }) => {
 		})
 	}
 	return (
-		<div id="header-add" className="d-flex flex-column">
-			<div className="d-flex justify-content-start">
-				<button className="btn btn-primary" onClick={addNewToggler}>
-					Add new contact{' '}
-				</button>
-			</div>
-			{animatedProps.map(
-				({ item, key, props }) =>
-					item && (
-						<animated.div
-							style={props}
-							key={key}
-							className="d-flex flex-column head-wrapper justify-content-center align-items-center"
-						>
-							<div className={'form-group d-flex align-items-center'}>
-								<label className={'pr-3'}>
-									Name<span className={'text-danger'}>&nbsp;*</span>
-								</label>
-								<input
-									type="text"
-									className={'form-control'}
-									value={form.name}
-									name={'name'}
-									onChange={handleData}
-									placeholder="John Doe"
-								/>
-							</div>
+			<div id="header-add" className="d-flex flex-column">
+				<div className="d-flex justify-content-start">
+					<button className="btn btn-primary" onClick={addNewToggler}>
+						Add new contact{' '}
+					</button>
+				</div>
+				{animatedProps.map(
+						({item, key, props}) =>
+								item && (
+										<animated.div
+												style={props}
+												key={key}
+												className="d-flex flex-column head-wrapper justify-content-center align-items-center"
+										>
+											<div className={'form-group d-flex align-items-center'}>
+												<label className={'pr-3'}>
+													Name<span className={'text-danger'}>&nbsp;*</span>
+												</label>
+												<input
+														type="text"
+														className={'form-control'}
+														value={form.name}
+														name={'name'}
+														onChange={handleData}
+														placeholder="John Doe"
+												/>
+											</div>
 
-							<div className={'form-group d-flex align-items-center'}>
-								<label className={'pr-3'}>
-									Phone<span className={'text-danger'}>&nbsp;*</span>
-								</label>
-								<input
-									type="text"
-									className={'form-control'}
-									required
-									value={form.phone}
-									name={'phone'}
-									onChange={handleData}
-									placeholder="8-800-555-35-35"
-								/>
-							</div>
-							<div className={'form-group d-flex align-items-center'}>
+											<div className={'form-group d-flex align-items-center'}>
+												<label className={'pr-3'}>
+													Phone<span className={'text-danger'}>&nbsp;*</span>
+												</label>
+												<input
+														type="text"
+														className={'form-control'}
+														required
+														value={form.phone}
+														name={'phone'}
+														onChange={handleData}
+														placeholder="8-800-555-35-35"
+												/>
+											</div>
+											<div className={'form-group d-flex align-items-center'}>
 								<textarea
-									type="text"
-									className={'form-control'}
-									size="40"
-									value={form.info}
-									name={'info'}
-									onChange={handleData}
-									placeholder="Additional information"
+										type="text"
+										className={'form-control'}
+										size="40"
+										value={form.info}
+										name={'info'}
+										onChange={handleData}
+										placeholder="Additional information"
 								/>
-							</div>
-							<div className={'form-group d-flex align-items-center'}>
-								{imgPreview && <img id={'previewImage'} src={imgPreview} />}
-								<label className={'btn btn-primary ml-3'} htmlFor="image_input">
-									Choose the image
-								</label>
+											</div>
+											<div className={'form-group d-flex align-items-center'}>
+												{imgPreview && <img id={'previewImage'} src={imgPreview}/>}
+												<label className={'btn btn-primary ml-3'} htmlFor="image_input">
+													Choose the image
+												</label>
 
-								<input
-									type="file"
-									className={''}
-									id="image_input"
-									accept="image/*"
-									placeholder="image"
-									onChange={handleImage}
-								/>
-							</div>
-							<button className="btn btn-success" onClick={addClickHandler}>
-								Add contact
-							</button>
-						</animated.div>
-					)
-			)}
-		</div>
+												<input
+														type="file"
+														className={''}
+														id="image_input"
+														accept="image/*"
+														placeholder="image"
+														onChange={handleImage}
+												/>
+											</div>
+											<button className="btn btn-success" onClick={addClickHandler}>
+												Add contact
+											</button>
+										</animated.div>
+								)
+				)}
+			</div>
 	)
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,165 +1,166 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import React, { useState } from 'react'
-import { animated, useTransition } from 'react-spring'
+import React, {useState} from 'react'
+import {animated, useTransition} from 'react-spring'
 import Modal from './Modal'
 import host from '../host'
 import './scss/style.scss'
-import { setContacts } from '../store/actionCreators'
-import { connect } from 'react-redux'
+import {setContacts} from '../store/actionCreators'
+import {connect} from 'react-redux'
 
-const Contact = ({ deleteClickHandler, ...props }) => {
-	const [state, setState] = useState({
-		isEditing: false,
-		contact: props.contact || {}
-	})
-	const editDataFetch = async contact => {
-		fetch(`${host}/contacts/update/${contact._id}`, {
-			method: 'PATCH',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(contact),
-		})
-			.then(response => response.json())
-			.then(response => console.log(response))
-	}
-	const editClickHandler = (event, contact) => {
-		if (state.isEditing) {
-			if (contact.name && contact.phone) {
-				setState({
-					...state,
-					isEditing: !state.isEditing
-				})
-				editDataFetch(contact)
-			}
-		} else {
-			setState({
-				...state,
-				isEditing: !state.isEditing
-			})
-		}
-	}
+const Contact = ({deleteClickHandler, ...props}) => {
+  const [state, setState] = useState({
+    isEditing: false,
+    contact: props.contact || {}
+  })
+  console.log(state.contact)
+  const editDataFetch = async contact => {
+    fetch(`${host}/contacts/update/${contact._id}`, {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(contact),
+    })
+        .then(response => response.json())
+        .then(response => console.log(response))
+  }
+  const editClickHandler = (event, contact) => {
+    if (state.isEditing) {
+      if (contact.name && contact.phone) {
+        setState({
+          ...state,
+          isEditing: !state.isEditing
+        })
+        editDataFetch(contact)
+      }
+    } else {
+      setState({
+        ...state,
+        isEditing: !state.isEditing
+      })
+    }
+  }
 
-	const handleForm = ({ target }) => {
-		const { name, value } = target
-		setState({
-			...state,
-			contact: {
-				...contact,
-				[name]: value
-			}
-		})
-	}
-	const transitions = useTransition(state.isEditing, null, {
-		from: {
-			opacity: 0,
-			position: 'absolute'
-		},
-		enter: {
-			opacity: 1,
-			position: 'relative'
-		},
-		leave: {
-			position: 'absolute',
-			opacity: 0
-		}
-	})
+  const handleForm = ({target}) => {
+    const {name, value} = target
+    setState({
+      ...state,
+      contact: {
+        ...contact,
+        [name]: value
+      }
+    })
+  }
+  const transitions = useTransition(state.isEditing, null, {
+    from: {
+      opacity: 0,
+      position: 'absolute'
+    },
+    enter: {
+      opacity: 1,
+      position: 'relative'
+    },
+    leave: {
+      position: 'absolute',
+      opacity: 0
+    }
+  })
 
-	const { contact } = state
-	return transitions.map(({ item, key, props }) => (
-		<div key={key} className="contact-wrapper position-relative">
-			{!item ? (
-				<animated.div
-					style={props}
-					className="pt-2 px-4 my-3 mr-5  d-flex flex-column justify-content-center align-items-between main-wrapper  w-100"
-				>
-					<div className="d-flex flex-row flex-wrap justify-content-between ">
-						<div className="image-wrapper">
-							<img alt="contact avatar" src={`${host}/${contact.image}`} />
-						</div>
-						<div className="contactInfo  d-flex align-items-center">
-							<div className="d-flex  flex-column">
-								<div className="text-center name">{contact.name}</div>
-								<div className="text-center phone">{contact.phone}</div>
-							</div>
-						</div>
-						<div className="actions d-flex pr-3 align-items-center justify-content-center">
-							<button className="mr-3 btn btn-info" onClick={editClickHandler}>
-								Edit
-							</button>
-							<button
-								data-target={'#exampleModal' + contact._id}
-								data-toggle="modal"
-								className="btn btn-danger"
-								type="button"
-							>
-								Delete
-							</button>
-							<Modal deleteClickHandler={deleteClickHandler} contact={contact} />
-						</div>
-					</div>
-					<div className="additionalInfo pt-3 pb-4">{contact.info}</div>
-				</animated.div>
-			) : (
-				<animated.div
-					style={props}
-					className=" pt-2 px-4 my-3 mr-5 d-flex flex-column  justify-content-center align-items-between main-wrapper w-100"
-				>
-					<div className="d-flex flex-row  justify-content-between ">
-						<div className="image-wrapper">
-							<img alt="contact avatar" src={`${host}/${contact.image}`} />
-						</div>
-						<div className="contactInfo  d-flex align-items-center">
-							<div className="d-flex  flex-column">
-								<div className="text-center name">
-									<input
-										type="text"
-										id={'name-input' + contact.id}
-										name={'name'}
-										className={'form-control'}
-										onChange={handleForm}
-										value={contact.name}
-									/>
-								</div>
-								<div className="text-center phone">
-									<input
-										type="text"
-										className={'form-control'}
-										onChange={handleForm}
-										name={'phone'}
-										defaultValue={contact.phone}
-									/>
-								</div>
-							</div>
-						</div>
-						<div className="actions d-flex pr-3 align-items-center justify-content-center">
-							<button className="mr-3 btn btn-success" onClick={e => editClickHandler(e, contact)}>
-								Save
-							</button>
-						</div>
-					</div>
-					<div className="additionalInfo d-flex  justify-content-center align-items-between">
+  const {contact} = state
+  return transitions.map(({item, key, props}) => (
+      <div key={key} className="contact-wrapper position-relative">
+        {!item ? (
+            <animated.div
+                style={props}
+                className="pt-2 px-4 my-3 mr-5  d-flex flex-column justify-content-center align-items-between main-wrapper  w-100"
+            >
+              <div className="d-flex flex-row flex-wrap justify-content-between ">
+                <div className="image-wrapper">
+                  <img alt="contact avatar" src={`${host}/${contact.image}`}/>
+                </div>
+                <div className="contactInfo  d-flex align-items-center">
+                  <div className="d-flex  flex-column">
+                    <div className="text-center name">{contact.name}</div>
+                    <div className="text-center phone">{contact.phone}</div>
+                  </div>
+                </div>
+                <div className="actions d-flex pr-3 align-items-center justify-content-center">
+                  <button className="mr-3 btn btn-info" onClick={editClickHandler}>
+                    Edit
+                  </button>
+                  <button
+                      data-target={'#exampleModal' + contact._id}
+                      data-toggle="modal"
+                      className="btn btn-danger"
+                      type="button"
+                  >
+                    Delete
+                  </button>
+                  <Modal deleteClickHandler={deleteClickHandler} contact={contact}/>
+                </div>
+              </div>
+              <div className="additionalInfo pt-3 pb-4">{contact.info}</div>
+            </animated.div>
+        ) : (
+            <animated.div
+                style={props}
+                className=" pt-2 px-4 my-3 mr-5 d-flex flex-column  justify-content-center align-items-between main-wrapper w-100"
+            >
+              <div className="d-flex flex-row  justify-content-between ">
+                <div className="image-wrapper">
+                  <img alt="contact avatar" src={`${host}/${contact.image}`}/>
+                </div>
+                <div className="contactInfo  d-flex align-items-center">
+                  <div className="d-flex  flex-column">
+                    <div className="text-center name">
+                      <input
+                          type="text"
+                          id={'name-input' + contact.id}
+                          name={'name'}
+                          className={'form-control'}
+                          onChange={handleForm}
+                          value={contact.name}
+                      />
+                    </div>
+                    <div className="text-center phone">
+                      <input
+                          type="text"
+                          className={'form-control'}
+                          onChange={handleForm}
+                          name={'phone'}
+                          defaultValue={contact.phone}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="actions d-flex pr-3 align-items-center justify-content-center">
+                  <button className="mr-3 btn btn-success" onClick={e => editClickHandler(e, contact)}>
+                    Save
+                  </button>
+                </div>
+              </div>
+              <div className="additionalInfo d-flex  justify-content-center align-items-between">
 						<textarea
-							type="text"
-							placeholder="Info"
-							name={'info'}
-							className={'form-control'}
-							onChange={handleForm}
-							value={contact.info}
-						/>
-					</div>
-				</animated.div>
-			)}
-		</div>
-	))
+                type="text"
+                placeholder="Info"
+                name={'info'}
+                className={'form-control'}
+                onChange={handleForm}
+                value={contact.info}
+            />
+              </div>
+            </animated.div>
+        )}
+      </div>
+  ))
 }
 
 const mapStateToProps = (state, props) => ({
-	...props,
-	contacts: state.contacts
+  ...props,
+  contacts: state.contacts
 })
 
 const mapDispatchToProps = dispatch => ({
-	setContacts: () => dispatch(setContacts())
+  setContacts: () => dispatch(setContacts())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Contact)
